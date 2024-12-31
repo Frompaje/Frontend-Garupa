e5c66027fd4447da7f8a5fbea529f9203c831881
Frompaje <yanedwards11@gmail.com>
2024-12-31 15:05:51 -0300

    fix:remove header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { Dashboard } from "./components/app/dashboard";
-import { MobileMenu } from "./components/header/menu-mobile";
 
 function App() {
   return (
     <div className="bg-gray-900 h-screen">
-      <MobileMenu />
       <Dashboard />
     </div>
   );
