b9d7fc2254b3bd647f8717b0f620c700d526bf53
Frompaje <yanedwards11@gmail.com>
2025-01-02 16:41:05 -0300

    feat: filter external_id

--- src/components/app/dashboard/index.tsx
+++ src/components/app/dashboard/index.tsx
@@ -1,6 +1,6 @@
-import { Filter } from "@/components/filter";
 import { TransferTable } from "../../transfer-table";
 import { CreateTransferDailog } from "@/components/create-transfer-dailog/create-transfer-dailog";
+import { SearchFilter } from "@/components/filter";
 
 export const Dashboard = () => {
   return (
@@ -8,8 +8,8 @@ export const Dashboard = () => {
       <div className="bg-white  rounded h-[780px]">
         <h1 className="text-gray-900 p-4 font-bold">Ultimos transações</h1>
 
-        <div className="flex items-center">
-          <Filter />
+        <div className="flex items-center gap-2">
+          <SearchFilter />
           <CreateTransferDailog />
         </div>
         <TransferTable />
